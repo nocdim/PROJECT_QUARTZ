9621e7f8d3a7e7e4538f572b5607963e69192aa3
nocdim <nokkudimu@gmail.com>
2023-07-13 16:18:33 +0300

    remembering stuff

--- web/client/src/App.tsx
+++ web/client/src/App.tsx
@@ -1,10 +1,11 @@
+// main page
 import React from 'react';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Hello</h1>
     </div>
   );
 }
---
+++ web/client/src/components/welcomingLogo/welcomingLogo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './welcomingLogo.css';
+
+function welcomingLogo() {
+    return (
+        <></>
+    )
+}
+
