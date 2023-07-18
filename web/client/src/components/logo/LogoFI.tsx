90afaa8b4f7aa445e9df933ebc5b4f0d18fd1ce9
nocdim <nokkudimu@gmail.com>
2023-07-18 11:17:59 +0300

    fixed the webpack v5 error

--- web/client/src/components/logo/LogoFI.tsx
+++ web/client/src/components/logo/LogoFI.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './logo.css';
+import './Logo.css';
 
 import { Link } from 'react-router-dom'
 
--- web/client/src/pages/WelcomePage/WelcomePageCFI.tsx
+++ web/client/src/pages/WelcomePage/WelcomePageCFI.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import WelcomingLogotype from '../../components/logo/WelcomingLogo/WelcomingLogoFI';
+
+import { HOME_ROUTE } from '../../utils/consts';
+let path = require('path');
+let logoImgSrc: string = __dirname;
+let additionalImgSrc: string = __dirname;
 
 const WelcomePageC = () => {
+    console.log(path)
     return (
-        <div>Welcome</div>
+        <WelcomingLogotype 
+            logoImgSrc={logoImgSrc}
+            additionalImgSrc={additionalImgSrc}
+            link={HOME_ROUTE}
+        />
     )
 }
 
