f039fe7159871b1e8dd6bf46827fc09ac40dbb09
nocdim <nokkudimu@gmail.com>
2023-07-19 17:44:37 +0300

    working on css designing logo animation and about section

--- web/client/src/components/About/AboutFI.tsx
+++ web/client/src/components/About/AboutFI.tsx
@@ -3,7 +3,8 @@ import './About.css';
 const About = () => {
     return (
         <div className='About'>
-            About
+            <div className='Shape'>
+            </div>
         </div>
     )
 }
